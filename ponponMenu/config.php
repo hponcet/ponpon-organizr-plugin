97564a091545e6c405fd686f470a8f8b38cb21a1
Hugues Poncet <hugues.poncet@teamto.com>
2024-10-13 14:02:26 +0200

    update plugin for loading

--- ponponMenu/config.php
+++ ponponMenu/config.php
@@ -3,7 +3,4 @@
  * Always include PLUGINNAME-enabled
  * Along with all your other settings from plugin.php
  */
-return array(
-	'PONPONMENU-enabled' => false,
-	'PONPONMENU-pluginAuth' => '4',
-);
+return [];
--- ponponMenu/plugin.php
+++ ponponMenu/plugin.php
@@ -1,21 +1,32 @@
 <?php
 // PLUGIN INFORMATION
-$GLOBALS['plugins']['ponponmenu'] = array( // Plugin Name
-	'name' => 'Ponpon Menu', // Plugin Name
-	'author' => 'hponcet', // Who wrote the plugin
+$GLOBALS['plugins']['Ponpon'] = array( // Plugin Name
+	'name' => 'Ponpon', // Plugin Name
+	'author' => 'ponpon', // Who wrote the plugin
 	'category' => 'Interface', // One to Two Word Description
 	'link' => '', // Link to plugin info
 	'license' => 'personal', // License Type use , for multiple
-	'idPrefix' => 'PONPONMENU', // html element id prefix (All Uppercase)
-	'configPrefix' => 'PONPONMENU', // config file prefix for array items without the hypen (All Uppercase)
+	'idPrefix' => 'PONPON', // html element id prefix (All Uppercase)
+	'configPrefix' => 'PONPON', // config file prefix for array items without the hypen (All Uppercase)
 	'version' => '1.0.0', // SemVer of plugin
-	'image' => 'data/plugins/test/logo.png', // 1:1 non transparent image for plugin
+	'image' => '', // 1:1 non transparent image for plugin
 	'settings' => false, // does plugin need a settings modal?
-	'bind' => true, // use default bind to make settings page - true or false
+	'bind' => false, // use default bind to make settings page - true or false
 	'api' => 'api/v2/plugins/test/settings', // api route for settings page (All Lowercase)
 	'homepage' => false // Is plugin for use on homepage? true or false
 );
 
 class TestPlugin extends Organizr
 {
+	public function _pluginGetSettings()
+	{
+		return [
+			'Sample Settings' => [
+				$this->settingsOption('html', 'HTML Note', ['html' => '<span lang="en">This is just a note</span>']),
+			],
+			'FYI' => [
+				$this->settingsOption('html', 'HTML Note', ['html' => '<span lang="en">This is just a note</span>']),
+			]
+		];
+	}
 }
